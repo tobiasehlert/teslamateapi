4f5c32fa0ff0624eed9fa353e4acf4502ab7f1c9
Tobias Lindberg <tobias.ehlert@gmail.com>
2021-03-02 19:54:06 +0100

    updating traefik configuration example updating README

--- src/webserver.go
+++ src/webserver.go
@@ -61,6 +61,7 @@ func main() {
 			v1.GET("/", func(c *gin.Context) {
 				c.JSON(http.StatusOK, gin.H{"message": "TeslaMateApi v1 runnnig..", "path": "/api/v1"})
 			})
+
 			v1.GET("/cars", TeslaMateAPICarsV1)
 			v1.GET("/cars/:CarID", TeslaMateAPICarsV1)
 			v1.GET("/cars/:CarID/charges", TeslaMateAPICarsChargesV1)
