ca8ce2c7145578f1d15bc05802099f97ca3cbc27
Julien <julien@citio.digital>
2024-10-01 01:05:01 +0200

    feat: ability to use third party Tesla API providers

--- src/v1_TeslaMateAPICarsCommand.go
+++ src/v1_TeslaMateAPICarsCommand.go
@@ -112,9 +112,9 @@ func TeslaMateAPICarsCommandV1(c *gin.Context) {
 
 	switch getCarRegionAPI(TeslaAccessToken) {
 	case ChinaAPI:
-		TeslaEndpointUrl = "https://owner-api.vn.cloud.tesla.cn"
+		TeslaEndpointUrl = getEnv("TESLA_API_URL", "https://owner-api.vn.cloud.tesla.cn")
 	default:
-		TeslaEndpointUrl = "https://owner-api.teslamotors.com"
+		TeslaEndpointUrl = getEnv("TESLA_API_URL", "https://owner-api.teslamotors.com")
 	}
 
 	client := &http.Client{}
